7e7d86874f06ed622297dde2e7c18fdeb9b07e29
Yashgit1728 <yashwanthbandaru1247@gmail.com>
2024-11-26 23:18:00 -0500

    html changed

--- FlaskIntroduction-master/app.py
+++ FlaskIntroduction-master/app.py
@@ -3,6 +3,8 @@ from models import db, Todo, Project
 import os
 from datetime import datetime
 from flask import current_app
+from sqlalchemy import asc, desc
+
 
 
 app = Flask(__name__)
@@ -90,16 +92,12 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
     
-@app.route('/delete_form/<int:id>')
-def Formdelete(id):
-    project_to_delete = Project.query.get_or_404(id)
-
-    try:
-        db.session.delete(project_to_delete)
-        db.session.commit()
-        return redirect('/final_project')
-    except:
-        return 'There was a problem deleting that task'
+@app.route('/delete_form/<int:id>', methods=['POST'])
+def delete_project(id):
+    project = Project.query.get_or_404(id)
+    db.session.delete(project)
+    db.session.commit()
+    return redirect('/final_project')  # Redirect back to the project list
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
@@ -124,6 +122,7 @@ def Formupdate(id):
 
     if request.method == 'POST':
         # Update fields from the form
+        project.task = request.form['pTask']
         project.category = request.form['task_type']
         project.priority = int(request.form['pr'])
         project.due_date = datetime.strptime(request.form['due_date'], '%Y-%m-%d').date()
@@ -150,6 +149,28 @@ def Formupdate(id):
             return f"There was an issue updating your project: {e}"
 
     return render_template('/final_project/Form_update.html', project=project)
+
+
+@app.route('/final_project')
+def table_route():
+    # Default sorting parameters
+    sort_column = request.args.get('sort_column', 'id')  # Default column to sort by
+    sort_order = request.args.get('sort_order', 'asc')  # Default sort order (ascending)
+
+    # Dynamically determine sort order
+    if sort_order == 'desc':
+        sort_query = getattr(Project, sort_column).desc()
+    else:
+        sort_query = getattr(Project, sort_column).asc()
+
+    # Query the database and sort
+    projects = Project.query.order_by(sort_query).all()
+
+    # Pass sorted data to the template
+    return render_template('Form.html', projects=projects, sort_column=sort_column, sort_order=sort_order)
+
+
+
 
 if __name__ == "__main__":
     with app.app_context():
